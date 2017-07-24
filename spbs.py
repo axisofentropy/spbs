c939ab24aa71a46233fa2c2913a961458947c135
Adam Vollrath <adam@endpoint.com>
2017-07-24 03:08:50 -0500

    First real attempt at scoring. Slow but promising!

--- spbs.py
+++ spbs.py
@@ -1,5 +1,6 @@
 from random import shuffle, choice
 from csv import DictReader, DictWriter
+from itertools import product, combinations
 
 names = [
 ]
@@ -23,6 +24,8 @@ with open('input.csv') as csvfile:
   bids[name] = row
   print name, 'bids for', bids[name]
 
+names = names[1:7]
+#shifts = shifts[1:5]
 print 'These applicants will bid for shifts:', ', '.join(names)
 print ''
 
@@ -35,6 +38,14 @@ roster = {
   shift : None for shift in shifts
 }
 
+def score(roster):
+ score_total = 0
+ for shift in roster.keys():
+  score = int(bids[roster[shift]][shift]) - 1
+  # print "Score", score, "for Shift", shift, '-', roster[shift]
+  score_total = score_total - score
+ return score_total
+
 def assign (name, shift, bid_index):
  roster[shift] = name
  #bids[name][shift] = 10000 + bid_index + 1
@@ -42,6 +53,37 @@ def assign (name, shift, bid_index):
  shifts.remove(shift)
  print "Assigned", name, 'to shift', shift, "in round", bid_index + 1
 
+rosters = [] # All possible rosters
+scores  = [] # Positional scores
+best_score = -100
+best_roster = {}
+
+iii = 0
+for roster in combinations(product(shifts, names), len(names)):
+ iii = iii + 1
+ if not iii % 1000000:
+  print "generated", iii
+ roster = dict(roster)
+ if len(set(roster)) == len(names) and len(set(roster.values())) == len(names):
+  this_score = score(roster)
+  #rosters.append(roster)
+  #scores.append(score(roster))
+  if this_score >= best_score:
+   best_score = this_score
+   print "found", this_score, roster
+   best_roster = roster
+
+"""
+for roster in combinations(pairings, len(names)):
+ candidate = dict(roster)
+ if len(candidate.keys()) == len(names):
+  print candidate
+  rosters.append(candidate)
+"""
+
+print len(rosters)
+
+"""
 for i in range(len(shifts)):
  print ''
  print "ROUND", i+1
@@ -53,18 +95,15 @@ for i in range(len(shifts)):
    assign(choice(bidders), shift, i)
   else:
    print "no bids this round for shift", shift
+"""
 
 print ''
 print 'ROSTER SCORE'
-score_total = 0
-for shift in roster.keys():
- score = int(bids[roster[shift]][shift]) - 1
- print "Score", score, "for Shift", shift, '-', roster[shift]
- score_total = score_total - score
-
-print "Roster score:", score_total
+print "Roster score:", score(best_roster)
 
 print 'OUTPUT'
+print best_roster
+
 #print fieldnames
 with open('/tmp/deleteme.csv', 'w') as csvfile:
  csvwriter = DictWriter(csvfile, fieldnames=csvreader.fieldnames)
