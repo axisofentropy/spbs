7b31e698bc0691803281b843c55a38b834b779b3
Adam Vollrath <adam@endpoint.com>
2017-08-10 22:23:25 -0500

    Big re-indentation

--- rostersearch.py
+++ rostersearch.py
@@ -2,82 +2,82 @@ from aima.search import Problem, hill_climbing
 from itertools import product
 
 class Roster(dict):
- def shifts(self):
-  return self.keys()
+    def shifts(self):
+        return self.keys()
 
- def names(self):
-  return self.values()
+    def names(self):
+        return self.values()
 
 class RosterProblem(Problem):
- def __init__(self, initial, bids, badgoals, goal=None):
-  """The constructor specifies the initial state, and possibly a goal
-  state, if there is a unique goal.  Your subclass's constructor can add
-  other arguments."""
-  self.initial = initial
-  self.goal = goal
-  self.bids = bids
-  self.badgoals = badgoals
-  self.names = bids.keys()
-  self.shifts = bids[list(bids.keys())[0]] #risky
+    def __init__(self, initial, bids, badgoals, goal=None):
+        """The constructor specifies the initial state, and possibly a goal
+        state, if there is a unique goal.  Your subclass's constructor can add
+        other arguments."""
+        self.initial = initial
+        self.goal = goal
+        self.bids = bids
+        self.badgoals = badgoals
+        self.names = bids.keys()
+        self.shifts = bids[list(bids.keys())[0]] #risky
 
- def actions(self, state):
-  """Return the actions that can be executed in the given
-  state. The result would typically be a list, but if there are
-  many actions, consider yielding them one at a time in an
-  iterator, rather than building them all at once."""
-  available_names = list(filter(lambda x: x not in state.names(), self.names))
-  available_shifts = list(filter(lambda x: x not in state.shifts(), self.shifts))
-  #print(list(product(available_shifts, available_names)))
-  return product(available_shifts, available_names)
+    def actions(self, state):
+        """Return the actions that can be executed in the given
+        state. The result would typically be a list, but if there are
+        many actions, consider yielding them one at a time in an
+        iterator, rather than building them all at once."""
+        available_names = list(filter(lambda x: x not in state.names(), self.names))
+        available_shifts = list(filter(lambda x: x not in state.shifts(), self.shifts))
+        #print(list(product(available_shifts, available_names)))
+        return product(available_shifts, available_names)
 
- def result(self, state, action):
-  """Return the state that results from executing the given
-  action in the given state. The action must be one of
-  self.actions(state)."""
-  output = Roster()
-  output.update(state)
-  output[action[0]] = action[1]
-  #print(output)
-  return output
+    def result(self, state, action):
+        """Return the state that results from executing the given
+        action in the given state. The action must be one of
+        self.actions(state)."""
+        output = Roster()
+        output.update(state)
+        output[action[0]] = action[1]
+        #print(output)
+        return output
 
- def goal_test(self, state):
-  if state in badgoals:
-   return False
-  return len(state.names) == len(self.names)
+    def goal_test(self, state):
+        if state in badgoals:
+            return False
+        return len(state.names) == len(self.names)
 
- # path_cost(self, c, state1, action, state2)
+    # path_cost(self, c, state1, action, state2)
 
- def value(self, state):
-  """For optimization problems, each state has a value.  Hill-climbing
-  and related algorithms try to maximize this value."""
-  score_total = 0 - (1000 * len(self.names))
-  for shift in state.keys():
-   score = int(self.bids[state[shift]][shift]) - 1
-   # print "Score", score, "for Shift", shift, '-', roster[shift]
-   score_total = score_total - score + 1000
-  #print("SCORE", score_total, state)
-  return score_total
+    def value(self, state):
+        """For optimization problems, each state has a value.  Hill-climbing
+        and related algorithms try to maximize this value."""
+        score_total = 0 - (1000 * len(self.names))
+        for shift in state.keys():
+            score = int(self.bids[state[shift]][shift]) - 1
+            # print "Score", score, "for Shift", shift, '-', roster[shift]
+            score_total = score_total - score + 1000
+        #print("SCORE", score_total, state)
+        return score_total
 
 def hill_climbing_roster_search(bids, shifts):
- problem = RosterProblem(Roster(), bids, badgoals=[])
- return hill_climbing(problem)
+    problem = RosterProblem(Roster(), bids, badgoals=[])
+    return hill_climbing(problem)
 
 def mountain_range_search(bids, shifts):
- # List of previously discovered goal states.
- attempts = 10000
- previous_goals = []
- while len(previous_goals) < attempts:
-  problem = RosterProblem(Roster(), bids, badgoals=previous_goals)
-  previous_goals.append(hill_climbing(problem))
-  print("finished run", len(previous_goals), "of", attempts)
+    # List of previously discovered goal states.
+    attempts = 10000
+    previous_goals = []
+    while len(previous_goals) < attempts:
+        problem = RosterProblem(Roster(), bids, badgoals=previous_goals)
+        previous_goals.append(hill_climbing(problem))
+        print("finished run", len(previous_goals), "of", attempts)
 
- high_score = float('-inf')
- best_roster = {}
- problem = RosterProblem(Roster(), bids, badgoals=None)
- for roster in previous_goals:
-  this_score = problem.value(roster)
-  if this_score > high_score:
-   #print("new best")
-   best_roster = roster
-   high_score = this_score
- return best_roster
+    high_score = float('-inf')
+    best_roster = {}
+    problem = RosterProblem(Roster(), bids, badgoals=None)
+    for roster in previous_goals:
+        this_score = problem.value(roster)
+        if this_score > high_score:
+            #print("new best")
+            best_roster = roster
+            high_score = this_score
+    return best_roster
--- spbs.py
+++ spbs.py
@@ -13,103 +13,103 @@ shifts = [
 ]
 
 def importcsvbids(csvfilename, bids):
- from csv import DictReader
- with open(csvfilename) as csvfile:
-  csvreader = DictReader(csvfile)
-  fieldnames = list(csvreader.fieldnames) # copy
+    from csv import DictReader
+    with open(csvfilename) as csvfile:
+        csvreader = DictReader(csvfile)
+        fieldnames = list(csvreader.fieldnames) # copy
 
-  for row in csvreader:
-   # Remove first field leaving just the bid numbers.
-   name = row[csvreader.fieldnames[0]]
-   row.pop(csvreader.fieldnames[0])
+        for row in csvreader:
+            # Remove first field leaving just the bid numbers.
+            name = row[csvreader.fieldnames[0]]
+            row.pop(csvreader.fieldnames[0])
 
-   # Add this person's bids.
-   bids[name] = row
-   print(name, 'bids for', bids[name])
+            # Add this person's bids.
+            bids[name] = row
+            print(name, 'bids for', bids[name])
 
-  return fieldnames
+        return fieldnames
 
 def score(roster):
- score_total = 0
- for shift in roster.keys():
-  score = int(bids[roster[shift]][shift]) - 1
-  score_total = score_total - score
- return score_total
+    score_total = 0
+    for shift in roster.keys():
+        score = int(bids[roster[shift]][shift]) - 1
+        score_total = score_total - score
+    return score_total
 
 def assign(name, shift, bid_index):
- roster[shift] = name
- #bids[name][shift] = 10000 + bid_index + 1
- names.remove(name)
- shifts.remove(shift)
- #print "Assigned", name, 'to shift', shift, "in round", bid_index + 1
+    roster[shift] = name
+    #bids[name][shift] = 10000 + bid_index + 1
+    names.remove(name)
+    shifts.remove(shift)
+    #print "Assigned", name, 'to shift', shift, "in round", bid_index + 1
 
 def exhaustive_search(bids, shifts):
- best_score = -100
- best_roster = {}
- roster_length = len(shifts)
- if roster_length is not len(bids.keys()):
-  raise Exception('not square bids.')
+    best_score = -100
+    best_roster = {}
+    roster_length = len(shifts)
+    if roster_length is not len(bids.keys()):
+        raise Exception('not square bids.')
 
- search_len = factorial(roster_length) * factorial(roster_length)
- print("Search through", search_len, "possibilities.")
+    search_len = factorial(roster_length) * factorial(roster_length)
+    print("Search through", search_len, "possibilities.")
 
- iii = 0
- for roster in product(
-  permutations(shifts, roster_length), permutations(bids.keys(), roster_length)
- ):
-  iii = iii + 1
-  percent = 100 * float(iii) / float(search_len)
-  if not percent % 1:
-   print(percent, "%  generated", iii, "of", search_len)
+    iii = 0
+    for roster in product(
+     permutations(shifts, roster_length), permutations(bids.keys(), roster_length)
+    ):
+        iii = iii + 1
+        percent = 100 * float(iii) / float(search_len)
+        if not percent % 1:
+            print(percent, "%  generated", iii, "of", search_len)
 
-  roster = zip(roster[0], roster[1])
-  roster = dict(roster)
-  this_score = score(roster)
-  if this_score > best_score:
-   best_score = this_score
-   print("found", this_score, roster)
-   best_roster = roster
+        roster = zip(roster[0], roster[1])
+        roster = dict(roster)
+        this_score = score(roster)
+        if this_score > best_score:
+            best_score = this_score
+            print("found", this_score, roster)
+            best_roster = roster
 
- return best_roster
+    return best_roster
 
 def exportcsvbids(csvfilename='/tmp/deleteme.csv', bids={}, fieldnames=[]):
- from csv import DictWriter
- with open(csvfilename, 'w') as csvfile:
-  csvwriter = DictWriter(csvfile, fieldnames=fieldnames)
-  csvwriter.writeheader()
-  for name in bids.keys():
-   output_row = {fieldnames[0]: name}
-   output_row.update(bids[name])
-   csvwriter.writerow(output_row)
-   #csvwriter.writerow({shift: bid for shift, bid in zip(output_fieldnames,[name,] + bids[name])})
+    from csv import DictWriter
+    with open(csvfilename, 'w') as csvfile:
+        csvwriter = DictWriter(csvfile, fieldnames=fieldnames)
+        csvwriter.writeheader()
+        for name in bids.keys():
+            output_row = {fieldnames[0]: name}
+            output_row.update(bids[name])
+            csvwriter.writerow(output_row)
+            #csvwriter.writerow({shift: bid for shift, bid in zip(output_fieldnames,[name,] + bids[name])})
 
 # Main loop if called via command-line
 if __name__ == "__main__":
- from argparse import ArgumentParser
+    from argparse import ArgumentParser
 
- argparser = ArgumentParser(
-  description='Simple Preferential Bidding System',
-  epilog='https://github.com/axisofentropy/spbs/'
- )
+    argparser = ArgumentParser(
+     description='Simple Preferential Bidding System',
+     epilog='https://github.com/axisofentropy/spbs/'
+    )
 
- argparser.add_argument("bidcsv",
-  help='Filename of input bids in CSV format.'
- )
+    argparser.add_argument("bidcsv",
+     help='Filename of input bids in CSV format.'
+    )
 
- args = argparser.parse_args()
+    args = argparser.parse_args()
 
- bids = {}
+    bids = {}
 
- fieldnames = importcsvbids(args.bidcsv, bids)
+    fieldnames = importcsvbids(args.bidcsv, bids)
 
- print('These applicants will bid for shifts:', ', '.join(bids.keys()))
+    print('These applicants will bid for shifts:', ', '.join(bids.keys()))
 
- shifts = bids[list(bids.keys())[0]] # risky
+    shifts = bids[list(bids.keys())[0]] # risky
 
- #best_roster = hill_climbing_roster_search(bids, shifts)
- best_roster = mountain_range_search(bids, shifts)
+    #best_roster = hill_climbing_roster_search(bids, shifts)
+    best_roster = mountain_range_search(bids, shifts)
 
- print("BEST", best_roster)
- print("SCORE", score(best_roster))
+    print("BEST", best_roster)
+    print("SCORE", score(best_roster))
 
- exportcsvbids(bids=bids, fieldnames=fieldnames)
+    exportcsvbids(bids=bids, fieldnames=fieldnames)
