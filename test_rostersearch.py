777dad034a27421d0eaacf41fa06c2c017a8c11d
Adam Vollrath <adam@endpoint.com>
2017-08-11 02:57:51 -0500

    Leverage `pytest` fixtures.

--- test_rostersearch.py
+++ test_rostersearch.py
@@ -3,6 +3,43 @@ import random
 
 from rostersearch import *
 
+@pytest.fixture
+def simple_initial():
+    initial = Roster()
+    initial['shift1'] = 'name2'
+    initial['shift2'] = 'name3'
+    initial['shift3'] = 'name1'
+    return initial
+
+@pytest.fixture
+def simple_goal():
+    goal = Roster()
+    goal['shift1'] = 'name1'
+    goal['shift2'] = 'name2'
+    goal['shift3'] = 'name3'
+    return goal
+
+@pytest.fixture
+def simple_bids():
+    bids = Bids({ #shuffled
+        'name2': {
+            'shift2': '1',
+            'shift3': '2',
+            'shift1': '3',
+        },
+        'name3': {
+            'shift2': '3',
+            'shift3': '1',
+            'shift1': '2',
+        },
+        'name1': {
+            'shift2': '2',
+            'shift3': '3',
+            'shift1': '1',
+        },
+    })
+    return bids
+
 def test_roster():
     r = Roster({
         'shift1': 'name1',
@@ -11,7 +48,7 @@ def test_roster():
     })
 
     # The .shifts() method should return iterables
-    # whose members match what we put in. 
+    # whose members match what we put in.
 
     assert len(r.shifts()) == 3
     assert list(r.shifts()).count('shift1') == 1
@@ -56,90 +93,46 @@ def test_bids():
     assert bids.list_names() == ['name1', 'name2', 'name3']
     assert bids.list_shifts() == ['shift1', 'shift2', 'shift3']
 
-def test_random_roster():
-    bids = Bids({ #shuffled
-        'name2': {
-            'shift2': '1',
-            'shift3': '2',
-            'shift1': '3',
-        },
-        'name3': {
-            'shift2': '3',
-            'shift3': '1',
-            'shift1': '2',
-        },
-        'name1': {
-            'shift2': '2',
-            'shift3': '3',
-            'shift1': '1',
-        },
-    })
+def test_random_roster(simple_bids):
 
     random.seed(23) # This should make results predictable.
 
-    roster1 = random_roster(bids)
+    roster1 = random_roster(simple_bids)
     assert roster1['shift1'] == 'name2'
     assert roster1['shift2'] == 'name1'
     assert roster1['shift3'] == 'name3'
 
-    roster2 = random_roster(bids)
+    roster2 = random_roster(simple_bids)
     assert roster2['shift1'] == 'name3'
     assert roster2['shift2'] == 'name2'
     assert roster2['shift3'] == 'name1'
-    
-    roster3 = random_roster(bids)
+
+    roster3 = random_roster(simple_bids)
     assert roster3['shift1'] == 'name2'
     assert roster3['shift2'] == 'name3'
     assert roster3['shift3'] == 'name1'
 
-def test_rosterproblem_init():
+class TestRosterProblem(object):
+    def test_rosterproblem_init(self, simple_initial, simple_goal, simple_bids):
 
-    initial = Roster()
-    initial['shift1'] = 'name2'
-    initial['shift2'] = 'name3'
-    initial['shift3'] = 'name1'
+        rp = RosterProblem(simple_initial, simple_bids, None, simple_goal)
 
-    goal = Roster()
-    goal['shift1'] = 'name1'
-    goal['shift2'] = 'name2'
-    goal['shift3'] = 'name3'
+        assert rp.initial == simple_initial
+        assert rp.goal == simple_goal
+        assert rp.bids == simple_bids
 
-    bids = Bids({ #shuffled
-        'name2': {
-            'shift2': '1',
-            'shift3': '2',
-            'shift1': '3',
-        },
-        'name3': {
-            'shift2': '3',
-            'shift3': '1',
-            'shift1': '2',
-        },
-        'name1': {
-            'shift2': '2',
-            'shift3': '3',
-            'shift1': '1',
-        },
-    })
+        # Derived values
 
-    rp = RosterProblem(initial, bids, None, goal)
+        assert len(rp.shifts) == 3
+        assert list(rp.shifts).count('shift1') == 1
+        assert list(rp.shifts).count('shift2') == 1
+        assert list(rp.shifts).count('shift3') == 1
 
-    assert rp.initial == initial
-    assert rp.goal == goal
-    assert rp.bids == bids
+        assert rp.shifts == simple_bids.list_shifts()
 
-    # Derived values
-    
-    assert len(rp.shifts) == 3
-    assert list(rp.shifts).count('shift1') == 1
-    assert list(rp.shifts).count('shift2') == 1
-    assert list(rp.shifts).count('shift3') == 1
+        assert len(rp.names) == 3
+        assert list(rp.names).count('name1') == 1
+        assert list(rp.names).count('name2') == 1
+        assert list(rp.names).count('name3') == 1
 
-    assert rp.shifts == bids.list_shifts()
-
-    assert len(rp.names) == 3
-    assert list(rp.names).count('name1') == 1
-    assert list(rp.names).count('name2') == 1
-    assert list(rp.names).count('name3') == 1
-
-    assert rp.names == bids.list_names()
+        assert rp.names == simple_bids.list_names()
